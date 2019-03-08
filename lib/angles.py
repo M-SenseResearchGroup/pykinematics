b1dd3494adacbc38a5e33f26b25dd54aaf47c999
Lukas Adamowicz <lukas.adamowicz@uvm.edu>
2019-03-08 15:31:53 -0500

    Updated angle calculation from axes to use the new tuple storage method

--- lib/angles.py
+++ lib/angles.py
@@ -17,10 +17,10 @@ def hip_from_frames(pelvis_AF, thigh_AF, R, side, zero_angles=False):
 
     Parameters
     ----------
-    pelvis_AF : numpy.ndarray
-        3x3 matrix of the pelvis anatomical axes in the pelvis sensor's frame. Columns correspond to pelvis X, Y, and Z.
-    thigh_AF : numpy.ndarray
-        3x3 matrix of the thigh anatomical axes in the thigh sensor's frame. Columns correspond to thigh X, Y, and Z.
+    pelvis_AF : tuple
+        Tuple of the x, y, and z axes of the pelvis anatomical frame.
+    thigh_AF : tuple
+        Tuple of the x, y, and z axes of the thigh anatomical frame.
     R : numpy.ndarray
         Nx3x3 array of rotation matrices from the thigh sensor frame to the pelvis sensor frame.
     side : {'left', 'right'}
@@ -35,14 +35,14 @@ def hip_from_frames(pelvis_AF, thigh_AF, R, side, zero_angles=False):
         second column being Ad / Abduction, and the third column being Internal - External Rotation.
     """
     # get pelvis anatomical frame
-    X = pelvis_AF[:, 0]
-    Y = pelvis_AF[:, 1]
-    Z = pelvis_AF[:, 2]
+    X = pelvis_AF[0]
+    Y = pelvis_AF[1]
+    Z = pelvis_AF[2]
 
     # get the thigh anatomical frame and rotate into pelvis frame
-    x = R @ thigh_AF[:, 0]
-    y = R @ thigh_AF[:, 1]
-    z = R @ thigh_AF[:, 2]
+    x = R @ thigh_AF[0]
+    y = R @ thigh_AF[1]
+    z = R @ thigh_AF[2]
 
     # form the joint axes
     e1 = Z.copy()
