f161252c703784d4266b9092b8826011647aac86
LukasAdamowicz <lukas.adamowicz95@gmail.com>
2019-10-18 15:48:58 -0400

    Add default_marker_names

--- pykinematics/omc/__init__.py
+++ pykinematics/omc/__init__.py
@@ -1,4 +1,4 @@
 from pykinematics.omc import angles
 from pykinematics.omc import calibration
 from pykinematics.omc import segmentFrames
-from pykinematics.omc.utility import MarkerNames
+from pykinematics.omc.utility import MarkerNames, default_marker_names
