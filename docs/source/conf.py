003381503666dd71db9e2cd20329b5e551f6e87e
LukasAdamowicz <lukas.adamowicz95@gmail.com>
2019-10-18 14:28:55 -0400

    Update theme Inlude __init__ documentation for classes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+autoclass_content = 'both'
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -78,7 +79,7 @@ modindex_common_prefix = ['pykinematics.']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
