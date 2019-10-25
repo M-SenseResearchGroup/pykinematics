f87f4c10c29f68f8b1f96bb2eef5221050d9defb
LukasAdamowicz <lukas.adamowicz95@gmail.com>
2019-10-25 19:02:48 -0400

    Adjustment to use parametrized fixtures to make cleaner tests

--- pykinematics/tests/conftest.py
+++ pykinematics/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from numpy import array
+from numpy import array, identity
 
 
 @pytest.fixture
@@ -18,3 +18,89 @@ def simple_x_mov_stats():
     sd = array([0.0, 0.0, 0.0, 3.36154726, 3.1144823, 1.58113883, 1.58113883, 1.4832397, 1.0, 1.67332005, 1.4832397,
                 2.64575131, 2.58843582, 2.88097206, 2.07364414, 2.77488739, 0.0, 0.0])
     return mn, sd
+
+
+@pytest.fixture
+def F():
+    return array([0.1, 0.2, 0.3, 0.4, 0.4, 0.3, 0.0, -0.5, -0.4, -0.35, -0.45, -0.8])
+
+
+@pytest.fixture(params=[2, 4])
+def derivative_result(request):
+    if request.param == 2:
+        res = array([1.0, 1.0, 1.0, 0.5, -0.5, -2.0, -4.0, -2.0, 0.75, -0.25, -2.25, -4.75])
+    elif request.param == 4:
+        res = array([1.25, 0.41666667, 1.75, 1.08333333, -0.41666667, -2.75, -5.33333333, -2.70833333, 0.625,
+                    -1.41666667, -0.70833333, -5.08333333])
+    return request.param, res
+
+
+@pytest.fixture(scope='module')
+def q1n():
+    return array([0.5, 0.5, 0.5, 0.5])
+
+
+@pytest.fixture(scope='module')
+def q2n():
+    return array([0.26, 0.13, 0.64, -0.71])
+
+
+@pytest.fixture(scope='module')
+def q3n():
+    return array([0.77, 0.46, 0.10, 0.43])
+
+
+@pytest.fixture(scope='module')
+def q4_2d():
+    return array([[1, 0, 0, 0], [0.26, 0.13, 0.64, -0.71]])
+
+
+@pytest.fixture(params=['12', '13', '23'])
+def qmult(request, q1n, q2n, q3n):
+    if request.param == '12':
+        res = array([0.1, -0.48, 0.87, 0.03])
+        return q1n, q2n, res
+    elif request.param == '13':
+        res = array([-0.11, 0.78, 0.45, 0.42])
+        return q1n, q3n, res
+    elif request.param == '23':
+        res = array([0.3817, 0.5659, 0.1363, -0.7163])
+        return q2n, q3n, res
+
+
+@pytest.fixture(params=['1', '2', '3', '4'])
+def qconj(request, q1n, q2n, q3n, q4_2d):
+    if request.param == '1':
+        return q1n, array([0.5, -0.5, -0.5, -0.5])
+    elif request.param == '2':
+        return q2n, array([0.26, -0.13, -0.64, 0.71])
+    elif request.param == '3':
+        return q3n, array([0.77, -0.46, -0.10, -0.43])
+    elif request.param == '4':
+        return q4_2d, array([[1, 0, 0, 0], [0.26, -0.13, -0.64, 0.71]])
+
+
+@pytest.fixture(params=['1', '2', '3'])
+def qinv(request, q1n, q2n, q3n):
+    if request.param == '1':
+        return q1n
+    elif request.param == '2':
+        return q2n
+    elif request.param == '3':
+        return q3n
+
+
+@pytest.fixture(params=['1', '2', '3', '4'])
+def q2mat(request, q1n, q2n, q3n, q4_2d):
+    if request.param == '1':
+        return q1n, array([[0.0,  0.0,  1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+    elif request.param == '2':
+        return q2n, array([[-0.8257546, 0.53511774, 0.14806656], [-0.2026174, -0.04106178, -0.97552084],
+                           [-0.51693413, -0.84044258, 0.14776805]])
+    elif request.param == '3':
+        return q3n, array([[0.61031696, -0.57002891, 0.5494351], [0.75397371, 0.20723794, -0.62221325],
+                           [0.24152751, 0.79416164, 0.55693298]])
+    elif request.param == '4':
+        return q4_2d, array([identity(3), [[-0.8257546, 0.53511774, 0.14806656],
+                                           [-0.2026174, -0.04106178, -0.97552084],
+                                           [-0.51693413, -0.84044258, 0.14776805]]])
--- pykinematics/tests/test_imu.py
+++ pykinematics/tests/test_imu.py
@@ -2,78 +2,49 @@
 Testing of functions and classes for IMU based estimation of joint kinematics
 """
 import pytest
-from numpy import array, allclose, isclose, random, identity, insert
+from numpy import isclose, random, insert
 
 from pykinematics.imu.utility import *
 
 
 class TestImuUtility:
-    @pytest.mark.parametrize(('x', 'order', 'result'), (
-            (array([0.1, 0.2, 0.3, 0.4, 0.4, 0.3, 0.0, -0.5, -0.4, -0.35, -0.45, -0.8]), 2,
-             array([1.0, 1.0, 1.0, 0.5, -0.5, -2.0, -4.0, -2.0, 0.75, -0.25, -2.25, -4.75])),
-            (array([0.1, 0.2, 0.3, 0.4, 0.4, 0.3, 0.0, -0.5, -0.4, -0.35, -0.45, -0.8]), 4,
-             array([1.25, 0.41666667, 1.75, 1.08333333, -0.41666667, -2.75, -5.33333333, -2.70833333, 0.625,
-                    -1.41666667, -0.70833333, -5.08333333]))))
-    def test_calc_derivative(self, x, order, result):
+    def test_calc_derivative(self, F, derivative_result):
         dt = 0.1
-        dx = calc_derivative(x, dt, order=order)
+        f = calc_derivative(F, dt, derivative_result[0])
 
-        assert allclose(dx, result)
+        assert allclose(f, derivative_result[1])
 
-    @pytest.mark.parametrize(('q1', 'q2', 'result'), (
-            (array([1, 0, 0, 0]), array([0.5, 0.5, 0.5, 0.5]), array([0.5, 0.5, 0.5, 0.5])),
-            (array([1, 0, 0, 0]), array([0.77, 0.46, 0.07, 0.43]), array([0.77, 0.46, 0.07, 0.43])),
-            (array([0.5, 0.5, 0.5, 0.5]), array([0.77, 0.46, 0.07, 0.43]), array([-0.095, 0.795, 0.435, 0.405])),
-            (array([0.77, 0.46, 0.07, 0.43]), array([0.77, 0.46, 0.07, 0.43]), array([0.1915, 0.7084, 0.1078, 0.6622])),
-    ))
-    def test_quat_mult(self, q1, q2, result):
-        assert allclose(quat_mult(q1, q2), result)
+    def test_quat_mult(self, qmult):
+        assert allclose(quat_mult(qmult[0], qmult[1]), qmult[2])
 
     @pytest.mark.parametrize(('q1', 'q2'), (
-            (array([0.77, 0.46, 0.07, 0.43]), array([1.3072, -0.455, 0.0312])),
-            (array([1.3072, -0.455, 0.0312]), array([0.77, 0.46, 0.07, 0.43])),
+            (random.rand(4), random.rand(3)),
+            (random.rand(3), random.rand(3)),
             (random.rand(4, 2), random.rand(4, 2)),
-            (random.rand(8), random.rand(8))))
+            (random.rand(8), random.rand(4))))
     def test_quat_mult_error(self, q1, q2):
         with pytest.raises(ValueError) as e_info:
             quat_mult(q1, q2)
 
-    @pytest.mark.parametrize(('q', 'result'), (
-            (array([1, 0.5, 0.3, -0.2]), array([1, -0.5, -0.3, 0.2])),
-            (array([[0.3, 0.5, -0.5, -0.8], [-0.5, 0.3, 0.1, 0.9]]), array([[0.3, -0.5, 0.5, 0.8],
-                                                                            [-0.5, -0.3, -0.1, -0.9]]))))
-    def test_quat_conj(self, q, result):
-        assert allclose(quat_conj(q), result)
+    def test_quat_conj(self, qconj):
+        assert allclose(quat_conj(qconj[0]), qconj[1])
 
-    @pytest.mark.parametrize(('q', ), (
-            (array([1, 1, 1, 1]),),
-            (random.rand(4),)))
-    def test_quat_inv(self, q):
-        qn = q / norm(q)
+    def test_quat_inv(self, qinv):
+        qn_ = qinv
+        q_ = qn_ * 3
 
-        qinv = quat_inv(q)
-        qninv = quat_inv(qn)
-
-        assert allclose(quat_mult(q, qinv), array([1, 0, 0, 0]))
-        assert allclose(quat_mult(qn, qninv), array([1, 0, 0, 0]))
-        assert isclose(norm(qninv), 1.0)
+        assert allclose(quat_mult(qn_, quat_inv(qn_)), array([1, 0, 0, 0]))
+        assert isclose(norm(qn_) * norm(quat_inv(qn_)), 1.0)
+        assert allclose(quat_mult(q_, quat_inv(q_)), array([1, 0, 0, 0]))
+        assert isclose(norm(q_) * norm(quat_inv(q_)), 1.0)
 
     def test_quat_inv_error(self):
         q = random.rand(2, 4)
         with pytest.raises(Exception):
             quat_inv(q)
 
-    @pytest.mark.parametrize(('q', 'result'), (
-            (array([1, 0, 0, 0]), identity(3)),
-            (array([0.26, 0.13, 0.64, -0.71]), array([[-0.8257546, 0.53511774, 0.14806656],
-                                                      [-0.2026174, -0.04106178, -0.97552084],
-                                                      [-0.51693413, -0.84044258, 0.14776805]])),
-            (array([[1, 0, 0, 0], [0.26, 0.13, 0.64, -0.71]]), array([identity(3),
-                                                                      [[-0.8257546, 0.53511774, 0.14806656],
-                                                                       [-0.2026174, -0.04106178, -0.97552084],
-                                                                       [-0.51693413, -0.84044258, 0.14776805]]]))))
-    def test_quat2matrix(self, q, result):
-        assert allclose(quat2matrix(q), result)
+    def test_quat2matrix(self, q2mat):
+        assert allclose(quat2matrix(q2mat[0]), q2mat[1])
 
     @pytest.mark.parametrize(('q', 'result'), (
             (array([[1, 0, 0, 0], [0.26, 0.13, 0.64, -0.71]]), array([0.79481443, 0.08177961, 0.40260729, -0.44664246])),
