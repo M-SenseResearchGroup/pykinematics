8b4ff019f8ce3a8b8e5711416c21edb1bb27a9c0
LukasAdamowicz <lukas.adamowicz95@gmail.com>
2019-11-19 21:38:20 -0500

    Remove code that should go in the general conftest

--- pykinematics/tests/imu/conftest.py
+++ pykinematics/tests/imu/conftest.py
@@ -4,25 +4,6 @@ import requests
 from tempfile import TemporaryFile
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--no-integration", action="store_true", default=False, help="Don't run integration testing"
-    )
-
-
-def pytest_configure(config):
-    config.addinivalue_line("markers", "integration: mark test as an integration test")
-
-
-def pytest_collection_modifyitems(config, items):
-    if not config.getoption('--no-integration'):
-        return  # --no-integration wasn't given, run all tests
-    skip_integration = pytest.mark.skip(reason="need --no-integration option to not run")
-    for item in items:
-        if "integration" in item.keywords:
-            item.add_marker(skip_integration)
-
-
 @pytest.fixture(scope='package')
 def sample_file():
     tf = TemporaryFile()  # temp file to store data
